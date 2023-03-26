ba8abaad464eeff3e3ab305ebc6be3662f99ecc6
Maryline Zhang <maryline.zhang@hotmail.com>
2023-03-26 18:38:07 +0200

    Timeout sur les test

--- src/test/java/app/map/TimeTest.java
+++ src/test/java/app/map/TimeTest.java
@@ -5,10 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class TimeTest {
+
+    private static final int DEFAULT_TIMEOUT = 2000;
 
     private void illegalArgumentHelper(int hour, int minute, int second) {
         assertThrows(IllegalArgumentException.class, () -> new Time(hour, minute, second),
@@ -17,23 +20,28 @@ public class TimeTest {
 
     @ParameterizedTest
     @ValueSource(ints = { -2, -1, 24, 25 })
+    @Timeout(DEFAULT_TIMEOUT)
     public void illegalNumberOfHour(int hour) {
         illegalArgumentHelper(hour, 0, 0);
     }
 
     @ParameterizedTest
     @ValueSource(ints = { -2, -1, 60, 61 })
+    @Timeout(DEFAULT_TIMEOUT)
     public void illegalNumberOfMinute(int minute) {
         illegalArgumentHelper(0, minute, 0);
     }
 
     @ParameterizedTest
     @ValueSource(ints = { -2, -1, 60, 61 })
+    @Timeout(DEFAULT_TIMEOUT)
+
     public void illegalNumberOfSecond(int second) {
         illegalArgumentHelper(0, 0, second);
     }
 
     @Test
+    @Timeout(DEFAULT_TIMEOUT)
     public void addDurationImmuable() {
         Time t1 = new Time(10, 11, 12);
         Time t2 = t1.addDuration(100);
@@ -48,26 +56,31 @@ public class TimeTest {
     }
 
     @Test
+    @Timeout(DEFAULT_TIMEOUT)
     public void addToNextMinute() {
         addDurationHelper(1, 1, 30, 45, 1, 2, 15);
     }
 
     @Test
+    @Timeout(DEFAULT_TIMEOUT)
     public void addMoreThan1minute() {
         addDurationHelper(23, 30, 15, 120, 23, 32, 15);
     }
 
     @Test
+    @Timeout(DEFAULT_TIMEOUT)
     public void addToNextHour() {
         addDurationHelper(12, 58, 20, 240 + 5, 13, 2, 25);
     }
 
     @Test
+    @Timeout(DEFAULT_TIMEOUT)
     public void addMoreThan1Hour() {
         addDurationHelper(14, 25, 10, 3600 + 1800 + 10, 15, 55, 20);
     }
 
     @Test
+    @Timeout(DEFAULT_TIMEOUT)
     public void addToNextDay() {
         addDurationHelper(23, 50, 0, 645, 0, 0, 45);
     }
