c3d6573149c2121f9ad828cd2a512e03df05ee6f
Maryline Zhang <maryline.zhang@hotmail.com>
2023-03-26 17:28:44 +0200

    Test avec des fonctions auxiliaire

--- src/test/java/app/map/TimeTest.java
+++ src/test/java/app/map/TimeTest.java
@@ -1,68 +1,74 @@
 package app.map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TimeTest {
-    @Test
-    public void checkPreconditionHourLt0() {
-        assertThrows(IllegalArgumentException.class, () -> new Time(-1, 0, 0));
+
+    private void illegalArgumentHelper(int hour, int minute, int second) {
+        assertThrows(IllegalArgumentException.class, () -> new Time(hour, minute, second),
+                String.format("Invalide values for times %02d:%02d:%02d", hour, minute, second));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { -2, -1, 24, 25 })
+    public void illegalNumberOfHour(int hour) {
+        illegalArgumentHelper(hour, 0, 0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { -2, -1, 60, 61 })
+    public void illegalNumberOfMinute(int minute) {
+        illegalArgumentHelper(0, minute, 0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { -2, -1, 60, 61 })
+    public void illegalNumberOfSecond(int second) {
+        illegalArgumentHelper(0, 0, second);
     }
 
     @Test
-    public void checkPreconditionHourGt23() {
-        assertThrows(IllegalArgumentException.class, () -> new Time(24, 0, 0));
+    public void addDurationImmuable() {
+        Time t1 = new Time(10, 11, 12);
+        Time t2 = t1.addDuration(100);
+        assertNotEquals(t1, t2, "Add duration return a new instance");
     }
 
-    @Test
-    public void checkPreconditionMinuteLt0() {
-        assertThrows(IllegalArgumentException.class, () -> new Time(0, -1, 0));
-    }
-
-    @Test
-    public void checkPreconditionMinuteGt59() {
-        assertThrows(IllegalArgumentException.class, () -> new Time(0, 60, 0));
-    }
-
-    @Test
-    public void checkPreconditionSecondLt0() {
-        assertThrows(IllegalArgumentException.class, () -> new Time(0, 0, -1));
-    }
-
-    @Test
-    public void checkPreconditionSecondGt59() {
-        assertThrows(IllegalArgumentException.class, () -> new Time(0, 0, 60));
-    }
-
-    @Test
-    public void midnight() {
-        assertEquals(new Time(0, 0, 0), new Time(0, 0, 0), "00:00:00");
-    }
-
-    @Test
-    public void add0sec() {
-        assertEquals(new Time(0, 0, 59), new Time(0, 0, 59).addDuration(0), "Add 0 second");
+    private void addDurationHelper(int hour1, int minute1, int second1, int duration, int hour2, int minute2,
+            int second2) {
+        Time t = new Time(hour1, minute1, second1).addDuration(duration);
+        Time expected = new Time(hour2, minute2, second2);
+        assertEquals(expected, t, t + " add " + duration + " seconds");
     }
 
     @Test
     public void addToNextMinute() {
-        assertEquals(new Time(0, 1, 0), new Time(0, 0, 59).addDuration(1), "Add to next minute");
+        addDurationHelper(1, 1, 30, 45, 1, 2, 15);
     }
 
     @Test
     public void addMoreThan1minute() {
-        assertEquals(new Time(0, 2, 0), new Time(0, 0, 59).addDuration(61), "Add more than one minute");
+        addDurationHelper(23, 30, 15, 120, 23, 32, 15);
     }
 
     @Test
     public void addToNextHour() {
-        assertEquals(new Time(1, 0, 0), new Time(0, 59, 59).addDuration(1), "Add to next hour");
+        addDurationHelper(12, 58, 20, 240 + 5, 13, 2, 25);
     }
 
     @Test
     public void addMoreThan1Hour() {
-        assertEquals(new Time(2, 0, 0), new Time(0, 59, 59).addDuration(3601), "Add to next hour");
+        addDurationHelper(14, 25, 10, 3600 + 1800 + 10, 15, 55, 20);
+    }
+
+    @Test
+    public void addToNextDay() {
+        addDurationHelper(23, 50, 0, 645, 0, 0, 45);
     }
 }
