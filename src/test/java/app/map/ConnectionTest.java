93dfd7f76437941fa3eb676a8030b981ecf4de9c
Maryline Zhang <maryline.zhang@hotmail.com>
2023-04-02 15:11:46 +0200

    Fix ConnectionText

--- src/test/java/app/map/ConnectionTest.java
+++ src/test/java/app/map/ConnectionTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.Timeout;
 public class ConnectionTest {
     private static final int DEFAULT_TIMEOUT = 2000;
 
-    private final Station station = new Station("test", 100, 200);
-    private final Connection connection = new Connection(station);
+    private final Station start = new Station("test", 100, 200);
+    private final Connection connection = new Connection(start);
 
     @Test
     @Timeout(DEFAULT_TIMEOUT)
@@ -18,14 +18,23 @@ public class ConnectionTest {
         assertThrows(IllegalArgumentException.class, () -> new Connection(null), "null station");
     }
 
-    private Section makeSection(String name, double x, double y) {
+    private Section makeSectionFromStart(String name, double x, double y) {
         Station start = new Station(name, x, y);
         Station arrival = new Station("", 0, 0);
         return new Section(start, arrival, 0, 0);
     }
 
+    private Section makeSectionFromArrival(String name, double x, double y) {
+        Station arrival = new Station(name, x, y);
+        return new Section(start, arrival, 0, 0);
+    }
+
+    private Section makeSectionFromArrival() {
+        return makeSectionFromArrival("", 0, 0);
+    }
+
     private void notStartingHereHelper(String name, double x, double y) {
-        Section section = makeSection(name, x, y);
+        Section section = makeSectionFromStart(name, x, y);
         assertThrows(IllegalArgumentException.class, () -> connection.addSection(section),
                 "Add a section not stating at this connection");
     }
@@ -42,15 +51,10 @@ public class ConnectionTest {
         notStartingHereHelper("test", 150, 250);
     }
 
-    private Section makeSection(Station start) {
-        Station arrival = new Station("", 0, 0);
-        return new Section(start, arrival, 0, 0);
-    }
-
     @Test
     @Timeout(DEFAULT_TIMEOUT)
     public void addSectionWithGoodStart() {
-        Section section = makeSection(station);
+        Section section = makeSectionFromArrival();
         connection.addSection(section);
         assertEquals(section, connection.getSections().get(0), "Add a section");
     }
@@ -58,8 +62,8 @@ public class ConnectionTest {
     @Test
     @Timeout(DEFAULT_TIMEOUT)
     public void addTwoDifferentSectionWithGoodStart() {
-        Section section1 = makeSection(station);
-        Section section2 = makeSection(station);
+        Section section1 = makeSectionFromArrival("a", 1, 2);
+        Section section2 = makeSectionFromArrival("b", 3, 4);
         connection.addSection(section1);
         connection.addSection(section2);
         assertEquals(2, connection.getSections().size(), "Add two sections");
@@ -68,9 +72,10 @@ public class ConnectionTest {
     @Test
     @Timeout(DEFAULT_TIMEOUT)
     public void addTwiceSameSectionWithGoodStart() {
-        Section section = makeSection(station);
-        connection.addSection(section);
-        connection.addSection(section);
+        Section section1 = makeSectionFromArrival();
+        Section section2 = makeSectionFromArrival();
+        connection.addSection(section1);
+        connection.addSection(section2);
         assertEquals(1, connection.getSections().size(), "Add the same section twice");
     }
 
