c9554f28ef71f23a8268f1e13f40ba8022fbf9fa
EruEri <ndiayeyves@gmail.com>
2023-04-08 06:29:12 +0200

    [Test]: Ajoute explicitement quelles stations sont obtenues

--- src/test/java/app/server/data/SuggestionStationsTest.java
+++ src/test/java/app/server/data/SuggestionStationsTest.java
@@ -2,6 +2,8 @@ package app.server.data;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
@@ -9,10 +11,12 @@ import org.junit.jupiter.api.Timeout;
 
 import app.map.LignedStation;
 import app.map.Map;
+import app.map.Station;
 
 public class SuggestionStationsTest {
 
     private static final String SUGESS_CRET = "Crét";
+    private static final String SUGESS_CHATELET = "Châtelet";
     private static final int DEFAULT_TIMEOUT = 2000;
 
     private static final String MAP_DATA_ALL = "map_data_all";
@@ -26,13 +30,37 @@ public class SuggestionStationsTest {
         return new SuggestionStations(prefix, stations);
     }
 
+    public static LignedStation createStation(String stationName, String Ligne) {
+        return new LignedStation(new Station(stationName, 0, 0), Ligne);
+    }
+
     @Test
     @Timeout(value = DEFAULT_TIMEOUT)
     public void testMultipleStationWithPrefix() throws Exception {
         SuggestionStations s = createSuggestionStations(SUGESS_CRET);
-        s.getStations().iterator().forEachRemaining(System.out::println);
-        assertTrue(s.getStations().size() == 3);
+        Set<LignedStation> set = s.getStations();
+        LignedStation creteilPref = createStation("Créteil - Préfecture", "8");
+        LignedStation creteilUni = createStation("Créteil - Université", "8");
+        LignedStation creteilEcha = createStation("Créteil - L'Échat", "8");
+
+        List<LignedStation> expected = Arrays.asList(creteilPref, creteilUni, creteilEcha);
+        assertTrue(set.size() == 3 && set.containsAll(expected));
+        
     }
 
+    @Test
+    @Timeout(value = DEFAULT_TIMEOUT)
+    public void testChatelet() throws Exception {
+        SuggestionStations s = createSuggestionStations(SUGESS_CHATELET);
+        Set<LignedStation> set = s.getStations();
+        LignedStation chatelet14 = createStation("Châtelet", "14");
+        LignedStation chatelet11 = createStation("Châtelet", "11");
+        LignedStation chatelet7 = createStation("Châtelet", "7");
+        LignedStation chatelet4 = createStation("Châtelet", "4");
+        LignedStation chatelet1 = createStation("Châtelet", "1");
+        
+        List<LignedStation> expected = Arrays.asList(chatelet1, chatelet4, chatelet7, chatelet11, chatelet14);
+        assertTrue(set.size() == 5 && set.containsAll(expected));
+    }
 
 }
