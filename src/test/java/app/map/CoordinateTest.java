c0590d9af34b3a1e96a079fec3d9ae1f1513fa82
Maryline Zhang <maryline.zhang@hotmail.com>
2023-05-02 14:41:31 +0200

    Gestion départ/arrivé à partir d'une coordonnée

--- src/test/java/app/map/CoordinateTest.java
+++ src/test/java/app/map/CoordinateTest.java
@@ -1,14 +1,14 @@
 package app.map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
 public class CoordinateTest {
     private static final int DEFAULT_TIMEOUT = 2000;
 
-    private void distanceHelper(double lat1, double long1, double lat2, double long2, double expected) {
+    private void distanceHelper(double lat1, double long1, double lat2, double long2,
+            double expected) {
         Coordinate c1 = new Coordinate(lat1, long1);
         Coordinate c2 = new Coordinate(lat2, long2);
         double res = c1.getDistance(c2);
@@ -24,24 +24,28 @@ public class CoordinateTest {
     @Test
     @Timeout(DEFAULT_TIMEOUT)
     public void testDistanceSameStation() {
-        distanceHelper(48.83866086365992, 2.2822419598550767, 48.83866086365992, 2.2822419598550767, 0);
+        distanceHelper(48.83866086365992, 2.2822419598550767, 48.83866086365992, 2.2822419598550767,
+                0);
     }
 
     @Test
     @Timeout(DEFAULT_TIMEOUT)
     public void testDistanceChatelet14and1() {
-        distanceHelper(48.85955653272677, 2.346411849769497, 48.85922471342816, 2.3457609541847755, 61);
+        distanceHelper(48.85955653272677, 2.346411849769497, 48.85922471342816, 2.3457609541847755,
+                60);
     }
 
     @Test
     @Timeout(DEFAULT_TIMEOUT)
     public void testDistanceGareDeLyon14and1() {
-        distanceHelper(48.8442498880687, 2.372519782814122, 48.8456832067358, 2.3731565937892047, 167);
+        distanceHelper(48.8442498880687, 2.372519782814122, 48.8456832067358, 2.3731565937892047,
+                166);
     }
 
     @Test
     @Timeout(DEFAULT_TIMEOUT)
     public void testDistanceBalardAndLourmel() {
-        distanceHelper(48.8442498880687, 2.278362661809200, 48.8456832067358, 2.3731565937892047, 6939);
+        distanceHelper(48.8442498880687, 2.278362661809200, 48.8456832067358, 2.3731565937892047,
+                6939);
     }
 }
